09cf71d83539b1e91472fd45d4dc6993a717db1e
Olga Martyanova <telanadas178@gmail.com>
2024-12-03 17:26:53 +0300

    added homework1

---
+++ src/test/java/TheInputsTest.java
@@ -0,0 +1,47 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class TheInputsTest {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setup() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-maximized");
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+
+    @Test
+    public void TheInputsTest() {
+        driver.get("https://the-internet.herokuapp.com/inputs");
+        WebElement input = driver.findElement(By.tagName("input"));
+        input.sendKeys("1");
+        input.sendKeys(Keys.ARROW_UP);
+        String actualValue = input.getAttribute("value");
+        Assert.assertEquals(actualValue, "2");
+        input.clear();
+        input.sendKeys("12");
+        input.sendKeys(Keys.ARROW_DOWN);
+        String actualValueDown = input.getAttribute("value");
+        Assert.assertEquals(actualValueDown, "11");
+        input.clear();
+        input.sendKeys("adc");
+        Assert.assertEquals(actualValue, "adc");
+    }
+    @AfterMethod(alwaysRun = true)
+    public void tearDown() {
+        driver.quit();
+    }
+}
